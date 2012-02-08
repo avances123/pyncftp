89d71ba57a95c686530dc7b62abbffecfa57d10b
Fabio Rueda <avances123@gmail.com>
2012-02-08 01:45:35 +0100

    First ftp success

--- src/pyncftp/net/ftp.py
+++ src/pyncftp/net/ftp.py
@@ -4,6 +4,8 @@ Created on 08/02/2012
 @author: Fabio Rueda <avances123@gmail.com>
 '''
 
+import ftplib
+
 class Ftp(object):
     '''
     classdocs
@@ -14,7 +16,17 @@ class Ftp(object):
         '''
         Constructor
         '''
-        pass
+        filename = 'test.txt'
+        f = open(filename,"rb")
+        conn = ftplib.FTP('avances123.dyndns.org')
+        conn.debug(1)  # See server messages
+        conn.set_pasv(False) # Control pasv connections
+        conn.login('fabio', 'fabiete123')
+        conn.storbinary('STOR ' + filename, f)  # basename and filehandler
+        f.close()
+        conn.quit()
 
 if __name__ == '__main__':
-    print 'Hello World'    
+    print 'Hello World'
+    a = Ftp()  
+    
