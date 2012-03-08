3b45aba8fc8d8cc163cb5c467f2b8216afb975d0
fabio <fabio@fa-work.(none)>
2012-03-08 16:23:59 +0100

    Arreglados fallos

--- src/pyncftp/tasks.py
+++ src/pyncftp/tasks.py
@@ -5,16 +5,16 @@ from random import choice
 import ftplib
 import json
 
-@task
-def enviaFTP(default_retry_delay=6): # Segundos
+@task(default_retry_delay=6,max_retries=2)
+def enviaFTP(): # Segundos
     try:
     	resultados = [0,1,2,3,4,5,6]
-    	tiempo = random.random()*20.0
+    	tiempo = random.random()*10
     	logger = enviaFTP.get_logger()
     	logger.warning("Haciendo una subida de FTP que dura %s segundos" % tiempo)
     	time.sleep(tiempo)
     	#return choice(resultados)
-	if tiempo >= 10:
+	if tiempo >= 2:
 	    raise ftplib.error_perm
 	    #raise Exception
 
